ba3617082ed270630037b9552d95a18bdf10485f
David Březina <david@rosettatype.com>
2022-06-03 08:56:41 +0200

    Bigger sharing image

--- docs/media/sharing-image.py
+++ docs/media/sharing-image.py
@@ -1,13 +1,13 @@
 txt = "Letter perception study"
 
-newPage(600, 300)
+newPage(800, 400)
 fill(0)
 stroke(None)
-rect(0, 0, 600, 300)
+rect(0, 0, 800, 600)
 fill(1)
 font("AdapterMonoPEVF-All")
 fontSize(50)
 fontVariations(wght=400)
-textBox(txt, (100, 40, 400, 200), align="center")
+textBox(txt, (200, 85, 400, 200), align="center")
 
 saveImage("sharing-image.png")
